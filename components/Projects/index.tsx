301e97734f597b8a387ab0d4f4d4a74ad807e7ff
Bruno Junqueira <bruno_cenu@hotmail.com>
2023-01-06 08:21:27 -0300

    Make project description appear on hover.

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -41,7 +41,7 @@ const Project = ({ background, name, type, tecs, link } : ProjectProps) => {
                 direction="column"
                 boxShadow="rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
                 display="block"
-                _hover={{display: flex}}
+                _hover={{display: "flex"}}
             >
                 <Text
                     fontWeight="black"
