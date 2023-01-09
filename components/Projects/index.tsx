ba70e9004c58fb77ee48037937d50facda75ffed
proman-wk <bruno_cenu@hotmail.com>
2023-01-09 05:20:14 -0300

    correction

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -3,117 +3,109 @@ import useBreakpoint from "contexts/useBreakpoint";
 
 import projects from "data/projects";
 import useTranslation from "next-translate/useTranslation";
+import { useState } from "react";
 
 export interface ProjectProps {
-    background: string
-    name: string
-    type: string
-    tecs: string []
-    link: string
+  background: string;
+  name: string;
+  type: string;
+  tecs: string[];
+  link: string;
 }
 
-const Project = ({ background, name, type, tecs, link } : ProjectProps) => {
+const Project = ({ background, name, type, tecs, link }: ProjectProps) => {
+  const [hover, setHover] = useState(false);
 
-    return(
-        <Flex
-            as={"a"}
-            direction="column"
-            h="15rem"
-            w="25rem"
-            bg={`url("${background}")`}
-            bgSize="cover"
-            bgPos="center"
-            justify="end"
-            borderRadius={5}
-            cursor="pointer"
-            _hover={{boxShadow: "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px"}}
-            overflow="hidden"
-            outline="1px solid"
-            outlineColor="invertedOutline"
-            onClick={ () => window.open(link, '_blank') }
-        >
-            <Flex
-                bg="bgColor"
-                w="100%"
-                h="6rem"
-                align="center"
-                justify="center"
-                direction="column"
-                boxShadow="rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
-                visibility="hidden"
-                _hover={{visibility: ""}}
-            >
-                <Text
-                    fontWeight="black"
-                    fontSize={24}
-                >
-                    { name }
-                </Text>
-                <Text
-
-                >
-                    { type }
-                </Text>
-                <Text
-                    fontSize={12}
-                >
-                    {tecs.map((tec, index)=>{
-                        if(index < 3 && index > 0){
-                            return ", " + tec;
-                        } else if(index === 0){
-                            return tec;
-                        }
-                    })}
-                    { (tecs.length > 3) && "..." }
-                </Text>
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      as={"a"}
+      direction="column"
+      h="15rem"
+      w="25rem"
+      bg={`url("${background}")`}
+      bgSize="cover"
+      bgPos="center"
+      justify="end"
+      borderRadius={5}
+      cursor="pointer"
+      _hover={{
+        boxShadow:
+          "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+      }}
+      overflow="hidden"
+      outline="1px solid"
+      outlineColor="invertedOutline"
+      onClick={() => window.open(link, "_blank")}
+      onMouseEnter={(e) => setHover(true)}
+      onMouseLeave={(e) => setHover(false)}
+    >
+      <Flex
+        bg="bgColor"
+        w="100%"
+        h="6rem"
+        align="center"
+        justify="center"
+        direction="column"
+        boxShadow="rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
+        opacity={hover ? 1 : 0}
+        transition="opacity 0.3s"
+      >
+        <Text fontWeight="black" fontSize={24}>
+          {name}
+        </Text>
+        <Text>{type}</Text>
+        <Text fontSize={12}>
+          {tecs.map((tec, index) => {
+            if (index < 3 && index > 0) {
+              return ", " + tec;
+            } else if (index === 0) {
+              return tec;
+            }
+          })}
+          {tecs.length > 3 && "..."}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default function Projects() {
+  const { t } = useTranslation("home");
 
-    const { t } = useTranslation('home');
+  const sizes = useBreakpoint();
 
-    const sizes = useBreakpoint();
-
-    return (
-        <Flex
-            as={"section"}
-            id="projects"
-            px={sizes.padding}
-            pt='6rem'
-            direction='column'
-            gap="2rem"
-        >
-            <Text
-                borderBottom='2px solid'
-                borderColor="prussianBlue"
-                w='fit-content'
-                fontSize={30}
-                fontWeight="black"
-            >
-                {t('projects')}
-            </Text>
-            <Flex
-                flexWrap="wrap"
-                gap="2rem"
-                align="center"
-                justify="center"
-            >
-                {projects.map((project, key)=>{
-                    return(
-                        <Project
-                            key={key}
-                            name={project.name}
-                            type={project.type}
-                            background={project.background}
-                            tecs={project.tecs}
-                            link={project.link}
-                        />
-                    )
-                })}
-            </Flex>
-        </Flex>
-    )
+  return (
+    <Flex
+      as={"section"}
+      id="projects"
+      px={sizes.padding}
+      pt="6rem"
+      direction="column"
+      gap="2rem"
+    >
+      <Text
+        borderBottom="2px solid"
+        borderColor="prussianBlue"
+        w="fit-content"
+        fontSize={30}
+        fontWeight="black"
+      >
+        {t("projects")}
+      </Text>
+      <Flex flexWrap="wrap" gap="2rem" align="center" justify="center">
+        {projects.map((project, key) => {
+          return (
+            <Project
+              key={key}
+              name={project.name}
+              type={project.type}
+              background={project.background}
+              tecs={project.tecs}
+              link={project.link}
+            />
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
 }
--- data/projects.ts
+++ data/projects.ts
@@ -1,48 +1,48 @@
 import { ProjectProps } from "components/Projects";
 
-const projects : ProjectProps [] = [
-    {
-        name: "Interesting",
-        type: "Website",
-        background: "interesting.png",
-        tecs: ["Next.js", "Javascript", "CSS"],
-        link: "http://interesting-photos.vercel.app/"
-    },
-    {
-        name: "Tasker Man(ager)",
-        type: "Node Module",
-        background: "taskerman.jpeg",
-        tecs: ["Typescript", "Node.js"],
-        link: "https://www.npmjs.com/package/tasker-man"
-    },
-    {
-        name: "Communicate",
-        type: "Website",
-        background: "communicate.jpeg",
-        tecs: ["Next.js", "Prisma", "Chakra-UI", "Javascript", "CSS"],
-        link: "https://github.com/brunojunqueira/communicate"
-    },
-    {
-        name: "Hackathon 2022",
-        type: "Website",
-        background: "hackathon.jpeg",
-        tecs: ["Python", "Typescript", "React.js", "Chakra-UI"],
-        link: "https://github.com/brunojunqueira/hackathon2022"
-    },
-    {
-        name: "Fireplace",
-        type: "Website",
-        background: "fireplace.jpeg",
-        tecs: ["Next.js", "Sass", "Adonis.js", "Javascript", "MySQL"],
-        link: "https://github.com/brunojunqueira/fireplace"
-    },
-    {
-        name: "Viniebrun Shop",
-        type: "Website",
-        background: "viniebrun.jpeg",
-        tecs: ["React.js", "React Redux", "Typescript", "Chakra-UI"],
-        link: "https://github.com/brunojunqueira/viniebrun-shop"
-    }
-]
+const projects: ProjectProps[] = [
+  {
+    name: "Interesting",
+    type: "Website",
+    background: "interesting.png",
+    tecs: ["Next.js", "Javascript", "CSS"],
+    link: "https://github.com/brunojunqueira/interesting",
+  },
+  {
+    name: "Tasker Man(ager)",
+    type: "Node Module",
+    background: "taskerman.jpeg",
+    tecs: ["Typescript", "Node.js"],
+    link: "https://www.npmjs.com/package/tasker-man",
+  },
+  {
+    name: "Communicate",
+    type: "Website",
+    background: "communicate.jpeg",
+    tecs: ["Next.js", "Prisma", "Chakra-UI", "Javascript", "CSS"],
+    link: "https://github.com/brunojunqueira/communicate",
+  },
+  {
+    name: "Hackathon 2022",
+    type: "Website",
+    background: "hackathon.jpeg",
+    tecs: ["Python", "Typescript", "React.js", "Chakra-UI"],
+    link: "https://github.com/brunojunqueira/hackathon2022",
+  },
+  {
+    name: "Fireplace",
+    type: "Website",
+    background: "fireplace.jpeg",
+    tecs: ["Next.js", "Sass", "Adonis.js", "Javascript", "MySQL"],
+    link: "https://github.com/brunojunqueira/fireplace",
+  },
+  {
+    name: "Viniebrun Shop",
+    type: "Website",
+    background: "viniebrun.jpeg",
+    tecs: ["React.js", "React Redux", "Typescript", "Chakra-UI"],
+    link: "https://github.com/brunojunqueira/viniebrun-shop",
+  },
+];
 
 export default projects;
