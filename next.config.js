ee027c3da72b03ab12ee22c9232a028b05121af4
proman-wk <bruno_cenu@hotmail.com>
2023-03-23 13:55:00 -0300

    added bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextTranslate = require('next-translate')
+const nextTranslate = require("next-translate");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   ...nextTranslate(),
-}
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig);
